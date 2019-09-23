d5158347d08294a8e20524a096fa418e52957df6
Ngheizit <ngheizitwu@163.com>
2019-09-23 21:44:16 +0800

    20190923 cshape add gdal

---
+++ gdal/GDAL_CShape/GDAL_CShape/FormMain.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace GDAL_CShape
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView_Dir = new System.Windows.Forms.TreeView();
+            this.tbx_showPath = new System.Windows.Forms.TextBox();
+            this.rtbx_spactialRef = new System.Windows.Forms.RichTextBox();
+            this.pictureBox_shp = new System.Windows.Forms.PictureBox();
+            this.rtbx_test = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.footlabel_xy = new System.Windows.Forms.ToolStripStatusLabel();
+            this.rtbx_loca = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_shp)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // treeView_Dir
+            // 
+            this.treeView_Dir.Location = new System.Drawing.Point(0, -1);
+            this.treeView_Dir.Name = "treeView_Dir";
+            this.treeView_Dir.Size = new System.Drawing.Size(200, 459);
+            this.treeView_Dir.TabIndex = 0;
+            this.treeView_Dir.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.TreeView_Dir_BeforeExpand);
+            this.treeView_Dir.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.TreeView_Dir_AfterExpand);
+            this.treeView_Dir.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView_Dir_AfterSelect);
+            // 
+            // tbx_showPath
+            // 
+            this.tbx_showPath.Location = new System.Drawing.Point(199, -1);
+            this.tbx_showPath.Name = "tbx_showPath";
+            this.tbx_showPath.ReadOnly = true;
+            this.tbx_showPath.Size = new System.Drawing.Size(763, 21);
+            this.tbx_showPath.TabIndex = 2;
+            // 
+            // rtbx_spactialRef
+            // 
+            this.rtbx_spactialRef.Location = new System.Drawing.Point(666, 267);
+            this.rtbx_spactialRef.Name = "rtbx_spactialRef";
+            this.rtbx_spactialRef.Size = new System.Drawing.Size(296, 191);
+            this.rtbx_spactialRef.TabIndex = 3;
+            this.rtbx_spactialRef.Text = "";
+            // 
+            // pictureBox_shp
+            // 
+            this.pictureBox_shp.BackColor = System.Drawing.Color.White;
+            this.pictureBox_shp.Location = new System.Drawing.Point(206, 26);
+            this.pictureBox_shp.Name = "pictureBox_shp";
+            this.pictureBox_shp.Size = new System.Drawing.Size(454, 429);
+            this.pictureBox_shp.TabIndex = 7;
+            this.pictureBox_shp.TabStop = false;
+            this.pictureBox_shp.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBox_shp_MouseMove);
+            // 
+            // rtbx_test
+            // 
+            this.rtbx_test.Location = new System.Drawing.Point(666, 26);
+            this.rtbx_test.Name = "rtbx_test";
+            this.rtbx_test.Size = new System.Drawing.Size(108, 27);
+            this.rtbx_test.TabIndex = 8;
+            this.rtbx_test.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.footlabel_xy});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 458);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(974, 22);
+            this.statusStrip1.TabIndex = 9;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // footlabel_xy
+            // 
+            this.footlabel_xy.Name = "footlabel_xy";
+            this.footlabel_xy.Size = new System.Drawing.Size(22, 17);
+            this.footlabel_xy.Text = "     ";
+            // 
+            // rtbx_loca
+            // 
+            this.rtbx_loca.Location = new System.Drawing.Point(666, 59);
+            this.rtbx_loca.Name = "rtbx_loca";
+            this.rtbx_loca.Size = new System.Drawing.Size(296, 202);
+            this.rtbx_loca.TabIndex = 8;
+            this.rtbx_loca.Text = "";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 480);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.rtbx_loca);
+            this.Controls.Add(this.rtbx_test);
+            this.Controls.Add(this.pictureBox_shp);
+            this.Controls.Add(this.rtbx_spactialRef);
+            this.Controls.Add(this.tbx_showPath);
+            this.Controls.Add(this.treeView_Dir);
+            this.Name = "FormMain";
+            this.Text = "GIS Catalog";
+            this.Load += new System.EventHandler(this.Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_shp)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView_Dir;
+        private System.Windows.Forms.TextBox tbx_showPath;
+        private System.Windows.Forms.RichTextBox rtbx_spactialRef;
+        private System.Windows.Forms.PictureBox pictureBox_shp;
+        private System.Windows.Forms.RichTextBox rtbx_test;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel footlabel_xy;
+        private System.Windows.Forms.RichTextBox rtbx_loca;
+    }
+}
+
---
+++ gdal/GDAL_CShape/GDAL_CShape/FormMain.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+using OSGeo.OGR;
+
+namespace GDAL_CShape
+{
+    public partial class FormMain : Form
+    {
+
+        private double m_pMinX, m_pMaxX, m_pMinY, m_pMaxY;
+
+
+
+        public FormMain()
+        {
+            InitializeComponent();
+            #region 驱动GDAL库
+            GdalConfiguration.ConfigureGdal();
+            GdalConfiguration.ConfigureOgr();
+            OSGeo.GDAL.Gdal.AllRegister(); 
+            #endregion
+        }
+
+        #region GDAL函数
+        private static string GetVector_SpactialRef(string filepath)
+        {
+            string info = "";
+            DataSource ds = Ogr.Open(filepath, 0);
+            if (ds == null)
+                return info = "Can not open " + filepath;
+            Driver drv = ds.GetDriver();
+            if (drv == null)
+                return info = "Can not get driver";
+            string srs_wkt = "";
+            for (int iLayer = 0; iLayer < ds.GetLayerCount(); iLayer++)
+            {
+                Layer layer = ds.GetLayerByIndex(iLayer);
+                if (layer == null)
+                    return info = "FAILURE: Couldn't fetch advertised layer" + iLayer;
+                OSGeo.OSR.SpatialReference sr = layer.GetSpatialRef();
+                if (sr != null)
+                    sr.ExportToPrettyWkt(out srs_wkt, 1);
+                else
+                    srs_wkt = "unKononw";
+            }
+            return "空间参考：\n" + srs_wkt;
+        }
+        private static string GetVector_GeomeInfo(string filepath)
+        {
+            string info = "";
+            DataSource ds = Ogr.Open(filepath, 0);
+            if (ds == null)
+                return info = "Can not open " + filepath;
+            Driver drv = ds.GetDriver();
+            if (drv == null)
+                return info = "Can not get driver";
+            for (int iLayer = 0; iLayer < ds.GetLayerCount(); iLayer++)
+            {
+                Layer layer = ds.GetLayerByIndex(iLayer);
+                if (layer == null)
+                {
+                    return info = "FAILURE: Couldn't fetch advertised layer" + iLayer;
+                }
+                Feature feat;
+                while ((feat = layer.GetNextFeature()) != null)
+                {
+                    Geometry geom = feat.GetGeometryRef();
+                    if (geom != null)
+                    {
+                        string geom_wkt;
+                        geom.ExportToWkt(out geom_wkt);
+                        info += geom_wkt + "\n";
+                    }
+                    feat.Dispose();
+                }
+            }
+            return info;
+
+        }
+        private static double[] GetVector_Extent(string filepath)
+        {
+            DataSource ds = Ogr.Open(filepath, 0);
+            Driver drv = ds.GetDriver();
+            double[] extent = new double[4];
+            for (int iLayer = 0; iLayer < ds.GetLayerCount(); iLayer++)
+            {
+                Layer layer = ds.GetLayerByIndex(iLayer);
+                Envelope ext = new Envelope();
+                layer.GetExtent(ext, 1);
+                extent[0] = ext.MinX;
+                extent[1] = ext.MaxX;
+                extent[2] = ext.MinY;
+                extent[3] = ext.MaxY;
+            }
+            return extent;
+        }
+        #endregion
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            #region 目录空间设置
+            foreach (string driver in Environment.GetLogicalDrives())
+            {
+                var dir = new DriveInfo(driver);
+                switch (dir.DriveType)
+                {
+                    case DriveType.Fixed:
+                        TreeNode tNode = new TreeNode(dir.Name.Split(':')[0]);
+                        tNode.Name = dir.Name;
+                        tNode.Tag = tNode.Name;
+                        this.treeView_Dir.Nodes.Add(tNode);
+                        tNode.Nodes.Add("");
+                        tNode.Expand();
+                        break;
+                }
+            } 
+            #endregion
+        }
+
+
+
+        #region 目录空间设置事件
+        private void TreeView_Dir_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string path = e.Node.Name;
+            tbx_showPath.Text = path;
+            if (path.Split('.')[path.Split('.').Length - 1] == "shp")
+            {
+                string geomeInfo = GetVector_GeomeInfo(path);
+                double[] extent = GetVector_Extent(path);
+                rtbx_spactialRef.Text = String.Format("MinX: {0}; \nMaxX: {1}; \nMinY: {2}; \nMaxY: {3}; \n", extent[0], extent[1], extent[2], extent[3]);
+                //rtbx_spactialRef.Text = geomeInfo;
+                this.m_pMinX = extent[0];
+                this.m_pMaxX = extent[1];
+                this.m_pMinY = extent[2];
+                this.m_pMaxY = extent[3];
+                string geomeType = geomeInfo.Split(' ')[0];
+                rtbx_test.Text = geomeType;
+
+                string str = "";
+                Graphics g = pictureBox_shp.CreateGraphics();
+                g.Clear(Color.White);
+                foreach (string sLocInfo in geomeInfo.Split(geomeType.ToCharArray()))
+                {
+                    if (sLocInfo != string.Empty)
+                    {
+                        str += sLocInfo;
+                        if (geomeType == "POINT")
+                        {
+                            string str_point = sLocInfo.Substring(2, sLocInfo.IndexOf(')') - 2);
+                            double x = Convert.ToDouble(str_point.Split(' ')[0]);
+                            double y = Convert.ToDouble(str_point.Split(' ')[1]);
+                            m2e(ref x, ref y);
+                            //MessageBox.Show(x.ToString() + ", " + y.ToString());
+                            Point pt = new Point((int)x, (int)y);
+                            //g.DrawLine(new Pen(Color.Black, 4), pt, new Point(pt.X + 4, pt.Y + 4));
+                            g.DrawEllipse(new Pen(Color.Black), pt.X, pt.Y, 4, 4);
+                            continue;
+                        }
+                        if(geomeType == "LINESTRING")
+                        {
+                            string str_points = sLocInfo.Substring(2, sLocInfo.IndexOf(')') - 2);
+                            int i = 0;
+                            Point fPt = new Point();
+                            foreach (string sLinePts in str_points.Split(','))
+                            {
+                                double x = Convert.ToDouble(sLinePts.Split(' ')[0]);
+                                double y = Convert.ToDouble(sLinePts.Split(' ')[1]);
+                                m2e(ref x, ref y);
+                                Point tPt = new Point((int)x, (int)y);
+                                if(i != 0)
+                                {
+                                    g.DrawLine(new Pen(Color.Black, 1), fPt, tPt);
+                                }
+                                fPt = tPt;
+                                i++;
+                            }
+                            continue;
+                        }
+                        if(geomeType == "POLYGON")
+                        {
+                            string str_points = "";
+                            try
+                            {
+                                str_points = sLocInfo.Substring(2, sLocInfo.LastIndexOf(')') - 2);
+                            }
+                            catch
+                            {
+                                continue;
+                            }
+                            List<Point> points = new List<Point>();
+                            int i = 0;
+                            foreach (string s in str_points.Split("),(".ToCharArray()))
+                            {
+                                if (s != string.Empty)
+                                {
+                                    double x = Convert.ToDouble(s.Split(' ')[0]);
+                                    double y = Convert.ToDouble(s.Split(' ')[1]);
+                                    m2e(ref x, ref y);
+                                    Point Pt = new Point((int)x, (int)y);
+                                    points.Add(Pt);
+                                    //if (i == 0)
+                                    //    startPoint = Pt;
+                                    //else
+                                    //{
+                                    //    if (startPoint.X == Pt.X && startPoint.Y == Pt.Y)
+                                    //        break;
+                                    //}
+                                }
+                                i++;
+                            }
+                            g.FillPolygon(Brushes.Black, points.ToArray(), System.Drawing.Drawing2D.FillMode.Winding);
+                            g.DrawLines(new Pen(Brushes.White, 1), points.ToArray());
+                        }
+                    }
+                }
+                rtbx_loca.Text = str;
+
+
+            }
+
+        }
+
+        private void TreeView_Dir_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            e.Node.Expand();
+        }
+
+        private void TreeView_Dir_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeViewItems.Add(e.Node);
+        }
+
+        #region TreeViewItems类
+        public static class TreeViewItems
+        {
+            public static void Add(TreeNode e)
+            {
+                try
+                {
+                    if (e.Tag.ToString() != "This PC")
+                    {
+                        e.Nodes.Clear();
+                        TreeNode tNode = e;
+                        string path = tNode.Name;
+                        string[] dics = Directory.GetDirectories(path);
+                        foreach (string dic in dics)
+                        {
+                            TreeNode subNode = new TreeNode(new DirectoryInfo(dic).Name);
+                            subNode.Name = new DirectoryInfo(dic).FullName;
+                            subNode.Tag = subNode.Name;
+                            tNode.Nodes.Add(subNode);
+                            subNode.Nodes.Add("");
+                        }
+                        string[] strFiles = Directory.GetFiles(path);
+                        foreach (string str in strFiles)
+                        {
+                            if (str.Split('.')[str.Split('.').Length - 1] == "shp")
+                            {
+                                TreeNode subNode = new TreeNode(Path.GetFileName(str));
+                                subNode.Name = Path.GetDirectoryName(str) + "\\" + Path.GetFileName(str);
+                                subNode.Tag = subNode.Name;
+                                tNode.Nodes.Add(subNode);
+                            }
+                        }
+                    }
+                }
+                catch (Exception msg)
+                {
+                    MessageBox.Show(msg.Message);
+                }
+            }
+        }
+        #endregion
+
+        #endregion
+
+
+        private void e2m(ref double x, ref double y)
+        {
+
+            double height = pictureBox_shp.Height;
+            double width = pictureBox_shp.Width;
+
+            double y_distance = this.m_pMaxY - this.m_pMinY;
+            double x_distance = this.m_pMaxX - this.m_pMinX;
+            if (x_distance > y_distance)
+                y_distance += (x_distance - y_distance);
+            else if (x_distance < y_distance)
+                x_distance += (y_distance - x_distance);
+
+            double x_persent = x / width;
+            double y_persent = y / height;
+
+            x = this.m_pMinX + (x_persent * x_distance);
+            y = this.m_pMinY + (y_persent * y_distance);
+
+            y = y_distance - y;
+        }
+        private void m2e(ref double x, ref double y)
+        {
+            double height = pictureBox_shp.Height;
+            double width = pictureBox_shp.Width;
+
+            double y_distance = this.m_pMaxY - this.m_pMinY;
+            double x_distance = this.m_pMaxX - this.m_pMinX;
+            if (x_distance > y_distance)
+                y_distance += (x_distance - y_distance);
+            else if (x_distance < y_distance)
+                x_distance += (y_distance - x_distance);
+
+            double x_persent = (x - this.m_pMinX) / x_distance;
+            double y_persent = (y - this.m_pMinY) / y_distance;
+
+            x = x_persent * width;
+            y = y_persent * height;
+
+            y = height - y;
+        }
+        private void PictureBox_shp_MouseMove(object sender, MouseEventArgs e)
+        {
+            double x = e.X;
+            double y = e.Y;
+            e2m(ref x, ref y);
+
+            string xys = "x = " + x + ", " + y;
+            footlabel_xy.Text = xys;
+        }
+    }
+}
