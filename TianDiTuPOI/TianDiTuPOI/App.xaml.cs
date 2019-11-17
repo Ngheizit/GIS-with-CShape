66ec0c56e1d6a06c96578a079a69dcedf75f400e
Ngheizit <ngheizitwu@163.com>
2019-11-17 22:17:43 +0800

    20191117

---
+++ TianDiTuPOI/TianDiTuPOI/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+using ESRI.ArcGIS.esriSystem;
+
+namespace TianDiTuPOI
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeEngineLicense();
+        }
+
+        private void InitializeEngineLicense()
+        {
+            AoInitialize aoi = new AoInitializeClass();
+
+            //more license choices could be included here
+
+            ESRI.ArcGIS.RuntimeManager.Bind(ESRI.ArcGIS.ProductCode.Desktop);
+
+            //esriLicenseProductCode productCode = esriLicenseProductCode.esriLicenseProductCodeEngine;
+            //if (aoi.IsProductCodeAvailable(productCode) == esriLicenseStatus.esriLicenseAvailable)
+            //{
+            //    aoi.Initialize(productCode);
+            //}
+        }
+    }
+}
---
+++ TianDiTuPOI/TianDiTuPOI/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Carto;
+
+namespace TianDiTuPOI
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AxLicenseControl licenseControl;
+        private AxMapControl mapControl;
+
+
+        private IMapControl2 m_pMapC2;
+        private IMapDocument m_pMapDoc;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateEngineControls();
+        }
+
+        private void CreateEngineControls()
+        {
+            licenseControl = new AxLicenseControl();
+            licenseHost.Child = licenseControl;
+
+            mapControl = new AxMapControl();
+            mapHost.Child = mapControl;
+        }
+
+        private void SetControlsProperty()
+        {
+            mapControl.OnMouseDown += new IMapControlEvents2_Ax_OnMouseDownEventHandler(mapControl_OnMouseMove);
+        }
+
+        private void mapControl_OnMouseMove(object sender, IMapControlEvents2_OnMouseDownEvent e)
+        {
+            if (e.button == 4)
+            {
+                m_pMapC2.MousePointer = esriControlsMousePointer.esriPointerPanning;
+                m_pMapC2.Pan();
+                m_pMapC2.MousePointer = esriControlsMousePointer.esriPointerArrow;
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetControlsProperty();
+            m_pMapC2 = mapControl.GetOcx() as IMapControl2;
+            m_pMapDoc = new MapDocumentClass();
+
+            LoadMxd();
+        }
+
+        private void LoadMxd(string mxdPath = @"./Map.mxd")
+        {
+            if (m_pMapC2.CheckMxFile(mxdPath))
+            {
+                m_pMapDoc.Open(mxdPath);
+                m_pMapC2.Map = m_pMapDoc.Map[0];
+            }
+        }
+    }
+}
